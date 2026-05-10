26f880e54f67170bd36b379957ddb76295321baa
antonie-riziki <antonriziki@gmail.com>
2026-05-10 17:43:56 +0300

    feat: add URL patterns for navigation stream and route guidance endpoints

--- sightra/apps/navigation/urls.py
+++ sightra/apps/navigation/urls.py
@@ -1,4 +1,5 @@
 from .views import NavigationStreamView, RouteGuidanceView
+from django.urls import path
 
 urlpatterns = [
     path("stream/", NavigationStreamView.as_view(), name="navigation_stream"),
