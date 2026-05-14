325fa2d726af89fc0e390c8491a7036cd6aec35b
antonie-riziki <antonriziki@gmail.com>
2026-05-14 09:05:49 +0300

    Debug set to True

--- sightra/sightra/settings.py
+++ sightra/sightra/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+0l!c+=wa&7#$+tczm7n225&7$$l#ahp)42pti5s!@zwyorv%w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
